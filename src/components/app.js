6df8714e84460bacce4c412c4f412600ef6356b5
aarondv1 <aarondv1@gmail.com>
2017-11-28 10:41:57 -0500

    Here it is

---
+++ src/components/app.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './app.css';
+
+class App extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            newDate : ''
+        };
+
+    }
+
+    changeBirthday(){
+        console.log(this.state);
+    }
+
+        render(){
+
+            return(
+                <div className="App">
+
+                    <Form inline>
+                        <h2>Input Your Birthday</h2>
+                        <FormControl type="date"
+                        onChange={event => this.setState({newDate: event.target.value})}
+                        >
+                        </FormControl>
+                            {' '}
+                        <Button onClick={() => this.changeBirthday()}>
+                            Submit
+                        </Button>
+                    </Form>
+
+                </div>
+            )
+        }
+
+}
+
+export default App;
